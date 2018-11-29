46bb76c00caf47bb469e074de0ab46d7715eaee2
sizin <sacar@excilys.com>
2018-11-29 15:22:21 +0100

    Using

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -29,8 +29,41 @@ angular.module('angularjsApp').controller('hideOrDisplayController', function($s
 });
 
 angular.module('angularjsApp').controller('helloWorldController', function($scope){
-  $scope.name = "Sinan"
+  $scope.name = "Sinan";
 
+});
+
+
+angular.module('angularjsApp').controller("calculateController", function($scope){
+  $scope.articles = [{
+    "name":"iPhone",
+    "quantity":1,
+    "price":30
+  },{
+    "name":"OnePlus",
+    "quantity":1,
+    "price":29.99
+  }]
+
+  $scope.total = function(){
+    var total = 0;
+
+    for(var i = 0; i < $scope.articles.length; i++){
+      total += $scope.articles[i].price * $scope.articles[i].quantity;
+    }
+
+    return total;
+  };
+
+  function calculateDiscount(newValue, oldValue, scope){
+    $scope.discount = (newValue > 100) ? newValue * 0.10 : 0;
+  };
+
+  $scope.finalTotal = function(){
+    return $scope.total() - $scope.discount;
+  }
+
+  $scope.$watch($scope.total, calculateDiscount);
 });
 
 
