fee42f96520089a6bd2d8a052abeccc3411e32be
sizin <sacar@excilys.com>
2018-12-03 15:46:59 +0100

    Creating filters

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -134,4 +134,20 @@ myModule.directive("expander", function(){
 });
 
 
+myModule.filter('reverse', function(){
+  return function(input, uppercase) {
+    input = input || '';
+    var out = "";
+    for(var i = 0; i < input.length; i++){
+      out = input.charAt(i) + out;
+    }
+    if(uppercase){
+      out = out.toUpperCase();
+    }
+    return out;
+  };
+});
 
+myModule.controller('myFilter', function($scope){
+  $scope.greeting = 'hello';
+});
